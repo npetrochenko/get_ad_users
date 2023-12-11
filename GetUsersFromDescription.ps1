3201a496686aa4ca072a26c2cc886641b8ba9c5b
Nikita <69148026+npetrochenko@users.noreply.github.com>
2023-12-11 20:43:15 +0300

    Add files via upload

---
+++ GetUsersFromDescription.ps1
@@ -0,0 +1,35 @@
+﻿
+### INITIATING TABLE ###
+
+$table = New-Object System.Data.DataTable "OwnersTable"
+$col1 = New-Object System.Data.DataColumn PC, ([string])
+$col2 = New-Object System.Data.DataColumn Username, ([string])
+$col3 = New-Object System.Data.DataColumn Mail, ([string])
+$col4 = New-Object System.Data.DataColumn Location, ([string])
+$col5 = New-Object System.Data.DataColumn Department, ([string])
+$col6 = New-Object System.Data.DataColumn Title, ([string])
+$table.Columns.Add($col1)
+$table.Columns.Add($col2)
+$table.Columns.Add($col3)
+$table.Columns.Add($col4)
+$table.Columns.Add($col5)
+$table.Columns.Add($col6)
+
+### GETTING DATA
+
+$PCList = Get-Content "D:\Scripts\GetUsers\comps.txt" |
+ForEach-Object {    
+    $PC = Get-ADComputer $_ -Properties Description, Location | Select Name, Description, Location #-Filter 'Enabled -eq $true' -Properties Description | Select Name, Description
+    $Username = $PC.Description.Split(";")[0]
+    $ADUser =  Get-ADUser $Username -Properties Mail, Title, Department
+    #$result += $PC.Name + ";" + $Username + ";" + $ADUser.Mail
+    $row = $table.NewRow()
+    $row.PC = $PC.Name
+    $row.Username = $Username
+    $row.Mail = $ADUser.Mail
+    $row.Location = $PC.Location
+    $row.Title = $ADUser.Title
+	$row.Department = $ADUser.Department
+    $table.Rows.Add($row)
+   }
+$table | Export-CSV 'D:\Scripts\GetUsers\owners.csv' -Encoding UTF8
---
+++ GetUsersFromQuser.ps1
@@ -0,0 +1,10 @@
+﻿$comps = Get-Content comps.txt
+ForEach ($comp in $comps) {
+    $quser_out = winrs -r:$comp quser
+    $split_line = $quser_out -split "`r`n"
+    for (($i = 1); $i -lt $split_line.Count; $i++)
+        {
+        $split_word = $($split_line[$i] -replace '\s', ' ').split()
+        Write-Host "$($split_word[1]); $($comp)"
+        } 
+}
